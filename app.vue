13ac00a32445de3c3f3315ab3469b810f9dbab5b
mahsa <mahsa.shatergholi@gmail.com>
2025-08-24 19:57:24 +0330

    test lint

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup lang="ts">
+const _test = 5
+console.log("test")
+</script>
---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import withNuxt from '@nuxt/eslint-config'
+
+export default withNuxt({
+  rules: {       
+    'no-console': 'error',
+    'no-unused-vars': 'error'  }
+})
